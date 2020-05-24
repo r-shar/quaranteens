c9a892980cfb989372b9e54e163ab2f6f04725f6
Charlize Dang <charlized18@berkeley.edu>
2020-05-24 15:31:34 -0700

    created select challenge view

---
+++ Quaranteen Project/SelectChallengeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelectChallengeViewController.swift
+//  Quaranteen Project
+//
+//  Created by Can Dang on 2020-05-24.
+//  Copyright © 2020 Rashmi Sharma. All rights reserved.
+//
+
+import UIKit
+
+class SelectChallengeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
