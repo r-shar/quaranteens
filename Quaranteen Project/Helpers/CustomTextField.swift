3a9def0605a23ec49df41365a9ff87c7df124f0b
Charlize Dang <charlized18@berkeley.edu>
2020-05-30 13:40:15 -0700

    added customtextfield helper

--- Quaranteen Project/Helpers/CustomTextField.swift
+++ Quaranteen Project/Helpers/CustomTextField.swift
@@ -27,4 +27,14 @@ class CustomTextField: UITextField {
         }
     }
     
+    @IBInspectable var inset: CGFloat = 0
+
+       override func textRect(forBounds bounds: CGRect) -> CGRect {
+           return bounds.insetBy(dx: inset, dy: inset)
+       }
+
+       override func editingRect(forBounds bounds: CGRect) -> CGRect {
+           return textRect(forBounds: bounds)
+       }
+    
 }
