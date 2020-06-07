5e34ed8db21f9f59512009345aaab704e300affa
Rajat Khare <rdkhare123@gmail.com>
2020-06-06 22:34:23 -0700

    edit profile VC change

--- Quaranteen Project/EditProfileViewController.swift
+++ Quaranteen Project/EditProfileViewController.swift
@@ -13,10 +13,15 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
 
 
     @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var email: UILabel!
     @IBOutlet weak var frogName: UILabel!
     @IBOutlet weak var profPic: UIImageView!
     
+    @IBOutlet weak var growth1: UIButton!
+    @IBOutlet weak var growth2: UIButton!
+    
+    let datePicker = UIDatePicker()
+    
+    @IBOutlet weak var birthday: UITextView!
     
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -25,12 +30,25 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
     @IBAction func done(_ sender: Any) {
         //save the information
         //save to database
+        var ref: DatabaseReference!
+        ref = Database.database().reference()
+        
+        guard let userID = Auth.auth().currentUser?.uid else { return }
+
+        ref.child("users").child(userID).updateChildValues(["name": name.text!, "imgURL": "img", "frogName" : frogName.text!])
         dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createDatePicker()
+        
+        growth1.layer.cornerRadius = 7
+        growth1.layer.masksToBounds = true
 
+        growth2.layer.cornerRadius = 7
+        growth2.layer.masksToBounds = true
+        
         // Do any additional setup after loading the view.
     }
     
@@ -51,8 +69,6 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
                        let imgURL = value?["imgURL"] as? String ?? ""
                        self.name.text = name
                        let url = URL(string: imgURL)
-                       let email = value?["email"] as? String ?? ""
-                       self.email.text = email
                        let frogName = value?["frogName"] as? String ?? ""
                        self.frogName.text = frogName
                        self.downloadImage(from: url!)
@@ -113,5 +129,29 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func createDatePicker() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        
+        toolbar.setItems([doneButton], animated: true)
+        
+        birthday.inputAccessoryView = toolbar
+        birthday.inputView = datePicker
+        datePicker.datePickerMode = .date
+    }
+    
+    @objc public func donePressed() {
+        
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        birthday.text = formatter.string(from: datePicker.date)
+        
+        self.view.endEditing(true)
+    }
 
 }
